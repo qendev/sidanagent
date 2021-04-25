f2c8ca965a13b7d16e9794c912cc36d57e55db8b
qendev <qendev00@gmail.com>
2021-04-25 12:23:42 +0300

    added map to location in signup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+          GMSServices.provideAPIKey("AIzaSyACm1zfEZtKj78i47eKVaru5Pc3HI2refA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_place_picker/google_maps_place_picker.dart';
 import 'package:sidan_agent/DashBoard.dart';
 import 'package:sidan_agent/DetailsSignup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -19,6 +21,9 @@ class SignUp extends StatefulWidget {
 
   const SignUp({Key key, this.user}) : super(key: key);
 
+  static final kInitialPosition = LatLng(-33.8567844, 151.213108);
+
+
 
 
   @override
@@ -31,6 +36,9 @@ class _SignUpState extends State<SignUp> {
 
 
   String email, password;
+
+  PickResult selectedPlace;
+
 
 
   TextEditingController firstnameController = TextEditingController();
@@ -267,7 +275,11 @@ class _SignUpState extends State<SignUp> {
                           style: BorderStyle.none,
                         ),
                       ),
-                      prefixIcon: Icon(Icons.location_on_rounded),
+                      prefixIcon: FlatButton(
+                        onPressed: (){
+                          openMap();
+                        },
+                          child: Icon(Icons.location_on_rounded)),
                       hintText: "Location",
                       hintStyle: TextStyle(color: Colors.grey.shade800),
                       filled: true,
@@ -408,6 +420,64 @@ class _SignUpState extends State<SignUp> {
       })
       );
     }
+  }
+
+  void openMap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return PlacePicker(
+            apiKey: "AIzaSyACm1zfEZtKj78i47eKVaru5Pc3HI2refA",
+            initialPosition: SignUp.kInitialPosition,
+            useCurrentLocation: true,
+            selectInitialPosition: true,
+
+            //usePlaceDetailSearch: true,
+            onPlacePicked: (result) {
+              selectedPlace = result;
+              Navigator.of(context).pop();
+              setState(() {});
+            },
+            //forceSearchOnZoomChanged: true,
+            //automaticallyImplyAppBarLeading: false,
+            //autocompleteLanguage: "ko",
+            //region: 'au',
+            //selectInitialPosition: true,
+            // selectedPlaceWidgetBuilder: (_, selectedPlace, state, isSearchBarFocused) {
+            //   print("state: $state, isSearchBarFocused: $isSearchBarFocused");
+            //   return isSearchBarFocused
+            //       ? Container()
+            //       : FloatingCard(
+            //           bottomPosition: 0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
+            //           leftPosition: 0.0,
+            //           rightPosition: 0.0,
+            //           width: 500,
+            //           borderRadius: BorderRadius.circular(12.0),
+            //           child: state == SearchingState.Searching
+            //               ? Center(child: CircularProgressIndicator())
+            //               : RaisedButton(
+            //                   child: Text("Pick Here"),
+            //                   onPressed: () {
+            //                     // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
+            //                     //            this will override default 'Select here' Button.
+            //                     print("do something with [selectedPlace] data");
+            //                     Navigator.of(context).pop();
+            //                   },
+            //                 ),
+            //         );
+            // },
+            // pinBuilder: (context, state) {
+            //   if (state == PinState.Idle) {
+            //     return Icon(Icons.favorite_border);
+            //   } else {
+            //     return Icon(Icons.favorite);
+            //   }
+            // },
+          );
+        },
+      ),
+    );
   }
 }
 
